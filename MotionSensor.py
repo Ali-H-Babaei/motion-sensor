7b0cfae5fdacafda2008a28e86f787850c7fb37a
Lee Wallen <leewallen@gmail.com>
2023-01-07 16:15:33 -0800

    Update to use list of sensors.

--- MotionSensor.py
+++ MotionSensor.py
@@ -1,10 +1,10 @@
-import requests
 import threading
+import requests
 
 
 class MotionSensor(object):
 
-    def __init__(self, hub_ip: str = None, username: str = None, interval: int = 15, sensor: int = 14):
+    def __init__(self, hub_ip: str = None, username: str = None, interval: int = 15, sensors: str = None):
         self.last_temp = None
         self.hub_ip = hub_ip or None
         self.username = username or None
@@ -15,38 +15,46 @@ class MotionSensor(object):
         if not username:
             raise ValueError('The username wasn''t provided.')
 
-        self.sensor = sensor
+        if not sensors:
+            raise ValueError("Please provide the motion sensors to monitor.")
+        else:
+            sensor_list = [int(x) for x in sensors.split(',')]
+
+        self.sensors = sensor_list
+        self.sensor_temps = {}
+        for sensor in self.sensors:
+            self.sensor_temps[sensor] = 0.0
+
         self.interval = interval
-        self.temperatures = {}
 
     def run(self):
         self.check_temp()
 
     def check_temp(self):
         threading.Timer(int(self.interval), self.check_temp).start()
-        response = requests.get(f'http://{self.hub_ip}/api/{self.username}/sensors/{self.sensor}', timeout=60)
 
-        if response.status_code != 200:
-            raise ValueError('Unable to get response from motion/temp sensor')
+        for sensor in self.sensors:
+            response = requests.get(f'http://{self.hub_ip}/api/{self.username}/sensors/{sensor}', timeout=60)
 
-        sensor_state = response.json()
+            if response.status_code != 200:
+                raise ValueError('Unable to get response from motion/temp sensor')
 
-        if not sensor_state['state']['temperature']:
-            return -99.99
+            sensor_state = response.json()
+            if not sensor_state['state']['temperature']:
+                return -99.99
 
-        temperature = self.get_temp_from_int(sensor_state['state']['temperature'])
-        if self.last_temp != temperature:
-            print(f'Temp changed! Temp: {temperature:.2f}')
-            self.last_temp = temperature
-        else:
-            print(f'Temp is unchanged. Temp: {temperature:.2f}')
-        return temperature
+            temperature = self.get_temp_from_int(sensor_state['state']['temperature'])
+
+            if self.sensor_temps[sensor] != temperature:
+                print(f'Temp changed! Temp: {temperature:.2f}')
+                self.sensor_temps[sensor] = temperature
+            else:
+                print(f'Temp is unchanged. Temp: {temperature:.2f}')
 
     @staticmethod
     def get_temp_from_int(temp: int = 0):
         # (0°C × 9/5) + 32 = 32°F
-        # temp is current temperature in 0.01 degrees Celsius.
-        float_val = float(temp/100.0)
+        float_val = float(temp / 100.0)
 
         fahrenheit = (float_val * (9 / 5)) + 32.0
         return fahrenheit
--- main.py
+++ main.py
@@ -7,10 +7,11 @@ if __name__ == '__main__':
     parser.add_argument("-b", "--bridge", help="Bridge, or hub, IP address")
     parser.add_argument("-u", "--username", help="Username created for interacting with the Hue Bridge")
     parser.add_argument("-i", "--interval", help="Interval in seconds to check motion sensor")
-    parser.add_argument("-s", "--sensor", help="Sensor to monitor")
+    parser.add_argument("-s", "--sensors", help="Comma separated string of sensor IDs to monitor")
     args = parser.parse_args()
 
     if args.bridge and args.username:
-        motion_sensor = MotionSensor(args.bridge, args.username, args.interval, args.sensor)
+        motion_sensor = MotionSensor(args.bridge, args.username, args.interval, args.sensors)
         motion_sensor.run()
+
 
